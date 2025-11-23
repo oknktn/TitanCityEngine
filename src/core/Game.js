f12fd77bedb3b9455205e9a6f2061a9719faf9f6
oknktn <oknktn@gmail.com>
2025-11-23 16:54:00 +0300

    Update Game.js

--- src/core/Game.js
+++ src/core/Game.js
@@ -2,17 +2,18 @@ import * as THREE from 'three';
 import { SceneManager } from './SceneManager.js';
 import { CameraManager } from './CameraManager.js';
 import { InteractionManager } from './InteractionManager.js';
-import { UIManager } from './UIManager.js'; // YENİ
+import { UIManager } from './UIManager.js';
 import { Grid } from '../world/Grid.js';
 import { BuildingManager } from '../world/BuildingManager.js';
+import { TrafficSystem } from '../systems/TrafficSystem.js'; // YENİ
 
 export class Game {
     constructor() {
         this.canvas = document.getElementById('game-canvas');
         this.isRunning = false;
         
-        // Varsayılan Alet
         this.activeTool = 'residential'; 
+        this.lastTime = 0; // DeltaTime için
 
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas,
@@ -26,14 +27,16 @@ export class Game {
 
         console.log("Titan Engine: Sistemler Yükleniyor...");
         
+        // SİSTEM KURULUMU (Sıralama Önemli!)
         this.sceneManager = new SceneManager(this);
         this.cameraManager = new CameraManager(this);
         this.grid = new Grid(200);
-        
         this.buildingManager = new BuildingManager(this);
         this.interactionManager = new InteractionManager(this);
         
-        // UI Manager en son (HTML elementlerine erişeceği için)
+        // YENİ: TRAFİK SİSTEMİ
+        this.trafficSystem = new TrafficSystem(this);
+        
         this.uiManager = new UIManager(this);
         
         window.addEventListener('resize', () => this.onResize());
@@ -49,15 +52,23 @@ export class Game {
             setTimeout(() => loader.remove(), 500);
         }
 
-        this.renderer.setAnimationLoop(() => this.loop());
+        this.renderer.setAnimationLoop((time) => this.loop(time)); // Loop'a zaman değişkeni veriyoruz
         console.log("Titan Engine: Hazır.");
     }
 
-    loop() {
+    loop(time) {
         if (!this.isRunning) return;
+
+        // Delta Time Hesapla (Animasyon hızını sabit tutmak için kritik)
+        const dt = (time - this.lastTime) / 1000;
+        this.lastTime = time;
 
         this.cameraManager.update();
         this.buildingManager.update();
+        
+        // TRAFİK GÜNCELLEMESİ
+        this.trafficSystem.update(dt);
+        
         this.renderer.render(this.sceneManager.scene, this.cameraManager.camera);
     }
 
